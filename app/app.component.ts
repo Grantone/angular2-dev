543cdf4e60a49e75ef01add97e20939c0c773388
Granton Mchana <gmchana254@gmail.com>
2017-09-13 14:59:42 +0300

    added task-list.component.ts file

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
+    <pies></pies>
+    `
+})
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>My First Angular 2 App</h1>
     <div *ngFor="Let currentTask of tasks">
     <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
 <button (click)="showDetails(currentTask)">Edit</button>
